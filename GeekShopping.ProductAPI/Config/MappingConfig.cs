9d25fdcd2db2ff35981c44248ef35556ff2a9b16
Mirael Ribeiro <Mirael Ribeiro@DESKTOP-LH27K2F>
2024-10-08 20:41:57 -0300

    primeira etapa incluindo CRUD na api

---
+++ GeekShopping.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObject;
+using GeekShopping.ProductAPI.Model;
+
+namespace GeekShopping.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mapping = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+            });
+
+            return mapping;
+        }
+    }
+}
---
+++ GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObject;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<ProductVO> FindById(long id);
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
+    }
+}
