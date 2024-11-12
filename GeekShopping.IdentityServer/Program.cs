c3260f473895f2cf0fc37996780c7c266805440a
Mirael Ribeiro <Mirael Ribeiro@DESKTOP-LH27K2F>
2024-11-11 23:05:59 -0300

    Adicionado a biblioteca Duente para integração e validação de usuario, criado o projeto identity server para adicionar essas configurações web

---
+++ GeekShopping.IdentityServer/Program.cs
@@ -0,0 +1,51 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.DataBaseModel.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connection = builder.Configuration.GetConnectionString("MySqlConnection");
+
+builder.Services.AddDbContext<MySQLContext>(options =>
+    options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 23))));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+var builderIdentity = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+.AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+.AddInMemoryClients(IdentityConfiguration.Clients)  
+.AddAspNetIdentity<ApplicationUser>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
