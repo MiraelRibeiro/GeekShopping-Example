8aebd66d4efdedae56dc59c9f157395000cd95a1
Mirael Ribeiro <Mirael Ribeiro@DESKTOP-LH27K2F>
2024-11-13 22:01:38 -0300

    Adicionado migrations das tabelas do banco, referencias de todas as portas de cada projeto, ajuste para uso de http e html nos programs

--- GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
+++ GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -6,7 +6,7 @@ namespace GeekShopping.IdentityServer.Configuration
     public static class IdentityConfiguration
     {
         public const string Admin = "Admin";
-        public const string Customer = "Customer";
+        public const string Client = "Client";
 
         public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>
         {
@@ -28,8 +28,8 @@ namespace GeekShopping.IdentityServer.Configuration
                 ClientId = "geek_shopping",
                 ClientSecrets = { new Secret("my_super_secret".Sha256())},
                 AllowedGrantTypes = GrantTypes.Code,
-                RedirectUris = {"http://localhost:60733/singin-oidc"},
-                PostLogoutRedirectUris = { "http://localhost:60733/signout-callback-oidc" },
+                RedirectUris = {"https://localhost:4430/singin-oidc"},
+                PostLogoutRedirectUris = { "https://localhost:4430/signout-callback-oidc" },
                 AllowedScopes = new List<string>
                 {
                     IdentityServerConstants.StandardScopes.OpenId,
